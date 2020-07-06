d3c7326295b5734816ca710c70123dd7976798ad
Alejandro Pinzon <jpinzon@healthcare.com>
2020-07-06 11:40:02 -0600

    Add Raygun Notifications
    Add Raygun Notifications

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,23 +1,22 @@
 package com.example;
 
+import com.mindscapehq.raygun4java.core.RaygunClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-//		try{
-//			SpringApplication.run(Application.class, args);
-//		}catch (Exception exception){
-//
-//			System.out.println("*********** Exception Handling ---> " + exception.getMessage());
-//
-//			throw  exception;
-//		}
-	}
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(Application.class, args);
+        } catch (Exception exception) {
+            final String raygunApiKey = System.getenv("RAYGUN_API_KEY");
+            new RaygunClient(raygunApiKey).send(exception);
+
+            throw exception;
+        }
+    }
 
 }
